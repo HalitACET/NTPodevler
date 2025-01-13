07619825399fcc145c38fbcc07355126bc69a27a
HalitACET <77839394+HalitACET@users.noreply.github.com>
2025-01-13 17:10:25 +0300

    Add files via upload

---
+++ tictactoe_/tictactoe_/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        public enum Player
+        {
+            X, O
+        }
+        Player currentPlayer;
+        List<Button> buttons;
+        Random rand = new Random();
+        int playerWins = 0;
+        int computerWins = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+        }
+
+        private void playerClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            currentPlayer = Player.X;
+            button.Text = currentPlayer.ToString();
+            button.Enabled = false;
+            button.BackColor = System.Drawing.Color.Cyan;
+            buttons.Remove(button);
+            Check();
+            timer1.Start();
+        }
+
+        private void AImove(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0)
+            {
+                int index = rand.Next(buttons.Count);
+                buttons[index].Enabled = false;
+                currentPlayer = Player.O;
+                buttons[index].Text = currentPlayer.ToString();
+                buttons[index].BackColor = System.Drawing.Color.DarkSalmon;
+                buttons.RemoveAt(index);
+                Check();
+                timer1.Stop();
+            }
+        }
+
+        private void restartGame(object sender, EventArgs e)
+        {
+            lblBot.Text = "Bot Kazanma Sayısı: ";
+            lblPlayer.Text = "Oyuncu Kazanma Sayısı: ";
+            resetGame();
+        }
+
+        private void loadbuttons()
+        {
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button9, button8 };
+        }
+
+        private void resetGame()
+        {
+            foreach (Control X in this.Controls)
+            {
+                if (X is Button && X.Tag == "play")
+                {
+                    ((Button)X).Enabled = true;
+                    ((Button)X).Text = "?";
+                    ((Button)X).BackColor = default(Color);
+                }
+            }
+            loadbuttons();
+            
+        }
+
+        private void Check()
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+               || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
+               || button7.Text == "X" && button9.Text == "X" && button8.Text == "X"
+               || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+               || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+               || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+               || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+               || button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                timer1.Stop();
+                MessageBox.Show("Kazandın");
+                playerWins++;
+                lblPlayer.Text = "Oyuncu Kazanma Sayısı: " + playerWins;
+                resetGame();
+            }
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+            || button4.Text == "O" && button5.Text == "O" && button6.Text == "O"
+            || button7.Text == "O" && button9.Text == "O" && button8.Text == "O"
+            || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+            || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
+            || button3.Text == "O" && button6.Text == "O" && button9.Text == "O"
+            || button1.Text == "O" && button5.Text == "O" && button9.Text == "O"
+            || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                timer1.Stop();
+                MessageBox.Show("Bot Kazandı");
+                computerWins++;
+                lblBot.Text = "Bot Kazandı: " + computerWins;
+                resetGame();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
