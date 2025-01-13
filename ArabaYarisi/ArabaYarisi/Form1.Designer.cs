e7b8ee05c8bd07d90a4dbbd301fc74f968859ce0
HalitACET <77839394+HalitACET@users.noreply.github.com>
2025-01-13 17:11:56 +0300

    Add files via upload

---
+++ ArabaYarisi/ArabaYarisi/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ArabaYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.araba1 = new System.Windows.Forms.PictureBox();
+            this.araba2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.araba1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(123, -15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 167);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(220, -15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 167);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(220, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 167);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(123, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 167);
+            this.label4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(220, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 167);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(123, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 167);
+            this.label6.TabIndex = 4;
+            // 
+            // araba1
+            // 
+            this.araba1.Image = ((System.Drawing.Image)(resources.GetObject("araba1.Image")));
+            this.araba1.Location = new System.Drawing.Point(12, 432);
+            this.araba1.Name = "araba1";
+            this.araba1.Size = new System.Drawing.Size(100, 117);
+            this.araba1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba1.TabIndex = 6;
+            this.araba1.TabStop = false;
+            // 
+            // araba2
+            // 
+            this.araba2.Image = ((System.Drawing.Image)(resources.GetObject("araba2.Image")));
+            this.araba2.Location = new System.Drawing.Point(272, 432);
+            this.araba2.Name = "araba2";
+            this.araba2.Size = new System.Drawing.Size(100, 117);
+            this.araba2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba2.TabIndex = 7;
+            this.araba2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.araba2);
+            this.Controls.Add(this.araba1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(400, 600);
+            this.MinimumSize = new System.Drawing.Size(400, 600);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.araba1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox araba1;
+        private System.Windows.Forms.PictureBox araba2;
+    }
+}
+
